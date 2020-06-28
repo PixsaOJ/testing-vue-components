1790e54c8d2003cf7e92d75c08a14e43f8515b5b
PixsaOJ <sabasaba101010@gmail.com>
2020-06-28 23:33:23 +0400

    exercise 1

--- specs/exercise-1.spec.js
+++ specs/exercise-1.spec.js
@@ -1,2 +1,28 @@
 import UserList from '@/exercise-1';
 import { mount } from '@vue/test-utils';
+import { name } from 'faker'
+
+test('component renders the users', () => {
+    const wrapper = mount(UserList, {
+        propsData: {
+            users: [ name.findName(), name.findName(), name.findName() ]
+        }
+    })
+    const li = wrapper.find('li')
+
+    // Exercise 1 part 1 make sure users get rendered
+    let userNameElems = wrapper.findAll('li');
+    expect(userNameElems.length).toBe(wrapper.props('users').length)
+
+    // Exercise 1 part 2 make sure filter works
+    const filter = wrapper.find('input')
+    filter.element.value = wrapper.props('users')[0]
+    filter.trigger('input')
+
+    wrapper.vm.$nextTick(() => {
+        expect(userNameElems.length).toBe(1)
+    })
+
+    expect(li.text()).toBe(wrapper.props('users')[0])
+
+})
